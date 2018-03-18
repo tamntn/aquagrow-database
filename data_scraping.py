ba5278f85d07e3f3385121c8425af542f4f58d4d
Tam Nguyen <tam.nguyen9779@gmail.com>
2018-03-18 18:41:09 -0500

    Update URL Scraping

--- data_scraping.py
+++ data_scraping.py
@@ -1,5 +1,6 @@
 import requests
 import csv
+import pprint
 from bs4 import BeautifulSoup
 
 uf_homepage = 'https://www.ufseeds.com'
@@ -11,17 +12,9 @@ burpee_urls = 'dataset/burpee_urls.csv'
 burpee_data = 'dataset/burpee_data.csv'
 
 
-# Create Urban Farmer Product URL File
-# Add Header Row
-with open(uf_urls, 'wb') as uf_urls_file:
-    writer = csv.writer(uf_urls_file)
-    headerRow = ['category', 'main', 'product', 'product_url']
-    writer.writerow(headerRow)
-
-
 # Add Product URL to file
-def UrbanFarmerAddUrlToFile(category, main, product, product_url):
-    row = [category, main, product, product_url]
+def UrbanFarmerAddUrlToFile(category, main, main_url, main_pic, product, product_url):
+    row = [category, main, main_url, main_pic, product, product_url]
     with open(uf_urls, 'a') as uf_urls_file:
         writer = csv.writer(uf_urls_file)
         writer.writerow(row)
@@ -30,42 +23,65 @@ def UrbanFarmerAddUrlToFile(category, main, product, product_url):
 
 # Scraping sub titles from each main titles
 # From Urban Farmer
-def UrbanFarmerGetProductUrl(category, title, titleURL):
-    r = requests.get(titleURL)
+def UrbanFarmerGetProductUrl(category, mainName, mainURL):
+    r = requests.get(mainURL)
     soup = BeautifulSoup(r.content, 'html.parser')
     try:
         main = soup.find("div", {"class": "shop-container"})
         descriptionDiv = main.find("div", {"class": "term-description"})
+        try:
+            mainPic = 'https:' + descriptionDiv.find("img").get('src')
+        except:
+            mainPic = 'N/A'
         productsDiv = main.find("div", {"class": "products"})
         productsText = main.findAll("div", {"class": "product-title"})
         for product in productsText:
             productName = product.find("a").get_text()
             productURL = 'https:' + product.find("a").get('href')
             print '|     |---' + productName
-            UrbanFarmerAddUrlToFile(category, title, productName, productURL)
-    except:
-        print '|     |---Error: Failed'
+            UrbanFarmerAddUrlToFile(category, mainName, mainURL, mainPic, productName, productURL)
+    except Exception as e:
+        print '|     |---Error:', e
 
 
 # Scraping main titles from each categories
 # From Urban Farmer
-def UrbanFarmerGetTitleUrl(category, categoryURL):
+def UrbanFarmerGetMainUrl(category, categoryURL):
     r = requests.get(categoryURL)
     soup = BeautifulSoup(r.content, 'html.parser')
     main = soup.find("div", {"class": "col-inner"})
-    URLsWithoutPics = main.find("div", {"class": "medium-3"})
+    URLsWithoutPics = main.find("div", {"class": "medium-3"}).findAll("li")
     URLsWithPics = main.find("div", {"class": "medium-9"}).findAll("div", {"class": "medium-4"})
-    for urlDiv in URLsWithPics:
-        titleURL = uf_homepage + urlDiv.find("a").get('href')
-        picURL = 'https:' + urlDiv.find("img").get('src')
-        title = urlDiv.find("p").get_text()
-        print '|---' + title
-        UrbanFarmerGetProductUrl(category, title, titleURL)
+    # Get Main URLs from The Div With Pictures (Not All Items)
+    # for urlDiv in URLsWithPics:
+    #     mainURL = uf_homepage + urlDiv.find("a").get('href')
+    #     picURL = 'https:' + urlDiv.find("img").get('src')
+    #     title = urlDiv.find("p").get_text()
+    #     print '|---' + title
+    #     UrbanFarmerGetProductUrl(category, title, mainURL)
+
+    # Get URLs from The Sidebar ( All Items)
+    for urlDiv in URLsWithoutPics:
+        try:
+            mainURL = uf_homepage + urlDiv.find("a").get('href')
+            title = urlDiv.find("a").get_text()
+            print '|---' + title
+            UrbanFarmerGetProductUrl(category, title, mainURL)
+        except:
+            print '|---Error: No Available URL'
 
 
 # Scraping all plants' URLs
 # From Urban Farmer
 def UrbanFarmerGetAllUrls(homepage):
+    # Create Urban Farmer Product URL File
+    # Add Header Row
+    with open(uf_urls, 'wb') as uf_urls_file:
+        writer = csv.writer(uf_urls_file)
+        headerRow = ['category', 'main', 'main_url', 'main_pic', 'product', 'product_url']
+        writer.writerow(headerRow)
+
+
     # Request Homepage
     r = requests.get(homepage)
     # Get homepage content
@@ -83,8 +99,8 @@ def UrbanFarmerGetAllUrls(homepage):
         categories_dict[category] = url
 
     for category in categories_dict:
-        print 'Processing:', category, '...'
-        UrbanFarmerGetTitleUrl(category, categories_dict[category])
+        print 'Processing:', category, '.......'
+        UrbanFarmerGetMainUrl(category, categories_dict[category])
 
 
 # Getting a plant's information
@@ -99,6 +115,35 @@ def UrbanFarmerGetPlantData(url):
         th = row.find('th').get_text().strip()
         td = row.find('td').get_text().strip()
         print th, ": ", td
+
+
+# Get all plants information
+# From Urban Farmers list of URLs
+def UrbanFarmer():
+    uf_data_output = {}
+    with open(uf_urls, 'rb') as uf_urls_file:
+        reader = csv.reader(uf_urls_file)
+        header = reader.next()
+        rows = [row for row in reader if row]
+        for row in rows:
+            category = row[0]
+            main = row[1]
+            main_url = row[2]
+            main_pic = row[3]
+            product = row[4]
+            product_url = row[5]
+            if (category in uf_data_output):
+                uf_data_output[category].append([{
+                    "main": main,
+                    "main_url": main_url
+                }])
+            else:
+                uf_data_output[category] = [{
+                    "main": main,
+                    "main_url": main_url
+                }]
+                
+    pprint.pprint(uf_data_output)
 
 
 # Getting a plant's information
@@ -116,5 +161,10 @@ def BurpeeGetPlantData(url):
         print category, ": ", description
 
 
+# UrbanFarmer()
 UrbanFarmerGetAllUrls(uf_homepage)
+
+# Processing Sprouts (Main) Separately Because of Urban Farmer's Site Error
+UrbanFarmerGetProductUrl('Vegetables', 'Sprouts', 'https://www.ufseeds.com/product-category/vegetables/sprout-seed/')
+
 # UrbanFarmerGetPlantData('https://www.ufseeds.com/product/sweet-g90-corn-seed/')
